4b4a8701f9bde27a630a3bf194524737934650cb
Alicia Cameron <cameron.a@belgiumcampus.ac.za>
2018-07-23 08:48:23 +0200

    Base design & functionality
    Right panel toggle needs styling. Form inputs for membership still needed. Buttons to be added.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function expand() {
+    // var target = $('.content-right__panel');
+    // target[0].classList.add('expanded');
+    // $( ".content-right__panel" ).slideToggle("slow", {direction: "left"}, 100);
+    $( ".content-right__panel" ).toggle("slide", 'slow');
+}
