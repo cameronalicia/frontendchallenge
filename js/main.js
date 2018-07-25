4f0777b9e0e8afa6cf8c3a2b236938ad3e2d1fac
Alicia Cameron <cameron.a@belgiumcampus.ac.za>
2018-07-25 13:25:44 +0200

    Mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 function expandRightPanel() {
-    $( ".content-right__panel" ).toggle("slide", "slow");
+
+    if ($(window).width() >= 1024) {
+        $( ".content-right__panel" ).toggle("slide", "slow"); //left -> right
+    } else {
+        $( ".content-right__panel" ).slideToggle('slow'); // top -> bottom
+
+        if (!$(".expand-arrow").hasClass("rotate")) {
+            $('html, body').animate({
+                scrollTop: $("#content-right").offset().top
+            }, 1000);
+        }
+    }
     $(".expand-arrow").toggleClass("rotate");
 }
