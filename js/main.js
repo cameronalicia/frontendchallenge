2eaafff6b0572719eecdf7ec0be1d39b162cf079
Alicia Cameron <cameron.a@belgiumcampus.ac.za>
2018-07-26 11:11:12 +0200

    Final changes & modifications

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 function expandRightPanel() {
-
     if ($(window).width() >= 1024) {
-        $( ".content-right__panel" ).toggle("slide", "slow"); //left -> right
+        $(".content-right__panel").toggle("slide", "slow"); //left -> right
     } else {
-        $( ".content-right__panel" ).slideToggle('slow'); // top -> bottom
+        $(".content-right__panel").slideToggle('slow'); // top -> bottom
 
         if (!$(".expand-arrow").hasClass("rotate")) {
             $('html, body').animate({
